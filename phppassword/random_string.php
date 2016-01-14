7ea4e394897a455423d5693eb3cc23bb2af5a04f
Richard Fernandez <kcomico@gmail.com>
2016-01-13 21:57:25 -0500

    update

--- phppassword/random_string.php
+++ phppassword/random_string.php
@@ -1,11 +1,6 @@
 <?php
   
-$lower = 'abcdefghijklmnopqrstuvwxyz';
-$upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-$numbers = '0123456789';
-$symbols = '$*?!-';
 
-$chars = $lower . $upper . $numbers . $symbols;
 
 function random_char($string) {
   $i = mt_rand(0, strlen($string)-1);
@@ -23,7 +18,33 @@ function random_string($length, $char_set) {
 return $output;
 }
 
-echo random_string(8, $chars);
+
+function generate_password($length) {
+	// define character sets
+	$lower = 'abcdefghijklmnopqrstuvwxyz';
+	$upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	$numbers = '0123456789';
+	$symbols = '$*?!-';
+
+	// extract config flags into variables
+	$use_lower = true;
+	$use_upper = false;
+	$use_numbers = true;
+	$use_symbol = true;
+
+	$chars = '';
+	if ($use_lower === true) { $chars .= $lower }
+	if ($use_upper === true) { $chars .= $upper }
+	if ($use_numbers === true) { $chars .= $numbers }
+	if ($use_symbol === true) { $chars .= $symbols }
+
+	$chars = $lower . $upper . $numbers . $symbols;
+
+	return random_string($length, $chars);
+}
+
+echo generate_password(8);
+ 
 
 
 ?>
